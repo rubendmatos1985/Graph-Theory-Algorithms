3644d4969724c8a5dab11f41ebf312fa776e8ea1
Ruben Dario <rubendmatos1985@gmail.com>
2019-10-11 12:10:18 +0200

    Understanding BFS algorithm
    Understanding BFS algorithm  in its original imperative way

---
+++ BreadthFirstSearch.js
@@ -0,0 +1,54 @@
+// ORIGINAL EXAMPLE AND ALGORITHM EXTRACTED FROM CORMEN'S BOOK
+// INTRODUCT TO ALGORITHMS
+//------------------------|
+//  r -- s      t - -u    |
+//  |    |    / |   / |   |
+//  |    |  /   | /   |   |
+//  v    w  - - x --  y   |
+//________________________|
+const graph={                                                        
+  Adj : {                                                            
+   r: [1, 4],                                                          
+   s: [0, 5],                                                        
+   t: [3,5,6],                                                       
+   u: [2,6,7],
+   v: [0],
+   w: [1,2,6],
+   x: [2,3,5,7],
+   y: [3,6]
+  },
+  V: [ "r", "s", "t", "u", "v", "w", "x", "y"]
+    .map( 
+      v => ({ value: v, color: "white", distance: 0, parent: null })) 
+};
+
+
+
+// BFS:: (G:Graph, s:Vertex) -> void
+let BFS = (G, s) =>{
+  let Q = [];
+  Q.push(s);
+  while(Q.length > 0)
+    {
+      let u = Q.shift();
+      G.Adj[u.value].forEach(
+        index => {
+            let v = G.V[index]; 
+            if(v.color == "white"){
+              v.color = "gray",
+              v.distance = u.distance + 1,
+              v.parent = u;
+              Q.push(v);
+            }
+         })
+      u.color = "black";
+    
+  }
+  G.V = [s, ...G.V]
+}
+
+BFS(graph, { value: "s", color: "gray", distance: 0, parent: null })
+
+
+
+
